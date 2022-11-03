1a9e75b1f7d3f66500194a49e27f4bdc400ace90
Charlie Robinson <45928293+charliedr@users.noreply.github.com>
2022-11-03 18:34:24 -0500

    Add files via upload

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,5 +1,5 @@
 import { FcGoogle } from "react-icons/fc";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../../utils/firebase";
 import { useRouter } from "next/router";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -13,7 +13,8 @@ export default function Login() {
   const googleProvider = new GoogleAuthProvider();
   const GoogleLogin = async () => {
     try {
-      const result = await signInWithPopup(auth, googleProvider);
+      // const result = await signInWithPopup(auth, googleProvider);
+      const result = await signInWithRedirect(auth, googleProvider);
       route.push("/");
     } catch (error) {
       console.log(error);
