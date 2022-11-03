dc0ba6eb1f3cac7bae9c4484611b836199c1ff4a
Charlie Robinson <45928293+charliedr@users.noreply.github.com>
2022-11-03 13:34:10 -0500

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,15 +15,15 @@ export default function Navbar() {
         <nav className="w-full fixed z-50 bottom-0 grid grid-cols-3 pt-3 pb-2 bg-white shadow-inner">
           <Link href="/" className="flex flex-col">
             <FaSign className="text-xl self-center mb-1 text-blue" />
-            <span className="text-sm font-medium">Yard Sales</span>
+            <span className="text-sm font-medium text-center">Yard Sales</span>
           </Link>
           <Link href="/add-listing" className="flex flex-col">
             <BsFillPlusCircleFill className="text-xl self-center mb-1 text-blue" />
-            <span className="text-sm font-medium">Add a Sale</span>
+            <span className="text-sm font-medium text-center">Add a Sale</span>
           </Link>
           <Link href="/roadmap" className="flex flex-col">
             <ImRoad className="text-xl self-center mb-1 text-blue" />
-            <span className="text-sm font-medium">Dev Roadmap</span>
+            <span className="text-sm font-medium text-center">Dev Roadmap</span>
           </Link>
         </nav>
       )}
